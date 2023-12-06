9caf79c18a2d1852fb5065c5f7da55d84849178c
Pushpendrayadav123 <305sonu12@gmail.com>
2023-12-07 01:40:48 +0530

    ascii valu find out

---
+++ Ascii.java
@@ -0,0 +1,10 @@
+import java.io.*;
+public class Ascii{
+    public static void main (String []args)
+    {
+        int a1='c';
+        int a2='d';
+        System.out.println("The ASCII value of c is: "+a1);  
+        System.out.println("The ASCII value of d is: "+a2);  
+    } 
+}
